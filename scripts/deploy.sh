e546b2c0e21af194bf0fc94654af6343fc68bff4
Thai Minh <minhthai40@gmail.com>
2016-05-03 09:45:26 +0700

    Edit deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,14 +5,22 @@
 
 # Send commands to the screen
 # Must not change the indentation below
-cmd="
-cd ~/webapp/url-shorten-bot/
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/git
-git pull
-export PAGE_TOKEN=EAAYtISKCzugBAInWeMCsGDrTO0tBrR54jJWIcPFZB0bQYoZAJxWWkHQEjbmr4WfKbLxItmAaIBsPWXLTrAvkTsAIrujjAo81xdx4ZBVM$
-export MYSQL_USERNAME=root
-export MYSQL_PASSWORD=123
-npm start
-"
-screen -X -S msg_bot stuff "${cmd}"
+# cmd="
+# cd ~/webapp/url-shorten-bot/
+# eval $(ssh-agent -s)
+# ssh-add ~/.ssh/git
+# git pull
+# export PAGE_TOKEN=EAAYtISKCzugBAInWeMCsGDrTO0tBrR54jJWIcPFZB0bQYoZAJxWWkHQEjbmr4WfKbLxItmAaIBsPWXLTrAvkTsAIrujjAo81xdx4ZBVM$
+# export MYSQL_USERNAME=root
+# export MYSQL_PASSWORD=123
+# npm start
+# "
+# screen -X -S "msg_bot" stuff "${cmd}"
+
+<<EOF
+    cd ~/webapp/url-shorten-bot/
+    eval $(ssh-agent -s)
+    ssh-add ~/.ssh/git
+    git pull
+    npm start
+EOF
