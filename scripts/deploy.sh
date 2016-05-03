986acbd2fe9f556c1225671d2129b4e23cb7f44e
Thai Minh <minhthai40@gmail.com>
2016-05-03 10:14:48 +0700

    No longer need deloy.sh because jenkins is removed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-# NOTE: This require a screen named 'msg_bot' running in 
-# background already!
-
-# Send commands to the screen
-# Must not change the indentation below
-# cmd="
-# cd ~/webapp/url-shorten-bot/
-# eval $(ssh-agent -s)
-# ssh-add ~/.ssh/git
-# git pull
-# export PAGE_TOKEN=EAAYtISKCzugBAInWeMCsGDrTO0tBrR54jJWIcPFZB0bQYoZAJxWWkHQEjbmr4WfKbLxItmAaIBsPWXLTrAvkTsAIrujjAo81xdx4ZBVM$
-# export MYSQL_USERNAME=root
-# export MYSQL_PASSWORD=123
-# npm start
-# "
-# screen -X -S "msg_bot" stuff "${cmd}"
 cd ~/webapp/url-shorten-bot/
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/git
